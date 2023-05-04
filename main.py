1548a4a9411d99c5f35be4b62d25fa2244f3a367
Aleksy <aslepaugo@gmail.com>
2023-05-04 08:46:21 +0200

    Add dialogflow

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from environs import Env
 from telegram import Update
 from telegram.ext import CallbackContext, Updater, CommandHandler, MessageHandler, Filters
 
+from dialogflow_utils import detect_intent_texts
+
 
 env = Env()
 env.read_env()
@@ -12,20 +14,29 @@ logger = logging.getLogger(__name__)
 
 
 def start(update: Update, context: CallbackContext):
-    update.message.reply_markdown_v2('Здравствуйте!')
+    update.message.reply_markdown_v2('Здравствуйте. Я бот, который поможет вам.')
     logger.info("Starting start()")
 
 
-def echo(update: Update, context: CallbackContext):
-    update.message.reply_text(update.message.text)
-    logger.info("Starting echo()")
+def reply(update: Update, context: CallbackContext):
+    try:
+        bot_reply = detect_intent_texts(
+            env("PROJECT_ID"),
+            update.message.from_user.id,
+            update.message.text,
+            env("LANGUAGE_CODE")
+        )
+        update.message.reply_text(bot_reply)
+    except Exception as e:
+        logger.error(e)
 
 
 def main():
     updater = Updater(env("TG_BOT_API_KEY"))
     dispatcher = updater.dispatcher
+
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, reply))
     updater.start_polling()
 
 
